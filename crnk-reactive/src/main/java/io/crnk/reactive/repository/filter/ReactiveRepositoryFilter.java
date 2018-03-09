2c8ada607607f60dbbcf7e42a12f19ac72aa9e41
Remo Meier <remo.meier@adnvoum.ch>
2018-03-09 09:11:01 +0100

    Support reactive/async programming #198
    initial interfaces

--- crnk-reactive/src/main/java/io/crnk/reactive/repository/filter/ReactiveRepositoryFilter.java
+++ crnk-reactive/src/main/java/io/crnk/reactive/repository/filter/ReactiveRepositoryFilter.java
@@ -2,21 +2,11 @@ package io.crnk.reactive.repository.filter;
 
 import io.crnk.core.engine.filter.RepositoryFilterContext;
 import io.crnk.core.repository.response.JsonApiResponse;
-import io.crnk.core.resource.links.LinksInformation;
-import io.crnk.core.resource.list.ResourceList;
-import io.crnk.core.resource.meta.MetaInformation;
 import reactor.core.publisher.Mono;
 
 public interface ReactiveRepositoryFilter {
 
-	Mono<JsonApiResponse> filterRequest(RepositoryFilterContext context, ReactiveRequestFilterChain chain);
+	Mono<JsonApiResponse> filterAccess(RepositoryFilterContext context, Mono<JsonApiResponse> response);
 
-	<T> Mono<ResourceList<T>> filterResult(RepositoryFilterContext context, ReactiveResultFilterChain<T> chain);
-
-	<T> Mono<MetaInformation> filterMeta(RepositoryFilterContext context, Iterable<T> resources, ReactiveMetaFilterChain
-			chain);
-
-	<T> Mono<LinksInformation> filterLinks(RepositoryFilterContext context, Iterable<T> resources, ReactiveLinksFilterChain
-			chain);
 
 }
--- crnk-reactive/src/main/java/io/crnk/reactive/repository/filter/ReactiveResultFilterChain.java
+++
@@ -1,9 +0,0 @@
-package io.crnk.reactive.repository.filter;
-
-import io.crnk.core.engine.filter.RepositoryFilterContext;
-
-public interface ReactiveResultFilterChain<T> {
-
-	Iterable<T> doFilter(RepositoryFilterContext context);
-
-}
